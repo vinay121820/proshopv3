5c0f1df3eccc3cb5202f07e1c312e6c0c06865a7
vinay121820 <155406122+vinay121820@users.noreply.github.com>
2025-02-11 19:27:35 +0530

    changed

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -1,22 +1,39 @@
 import { Table, Button, Row, Col } from 'react-bootstrap';
 import { FaEdit, FaPlus, FaTrash } from 'react-icons/fa';
-import { Link} from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import Message from '../../components/Message';
 import Loader from '../../components/Loader';
-import { useParams } from 'react-router-dom';
 import Paginate from '../../components/Paginate';
 import {
-  useGetProductsQuery,useCreateProductMutation,useDeleteProductMutation } from '../../slices/productsApiSlice';
+  useGetProductsQuery,
+  useDeleteProductMutation,
+  useCreateProductMutation,
+} from '../../slices/productsApiSlice';
 import { toast } from 'react-toastify';
 
 const ProductListScreen = () => {
-  const {pageNumber}=useParams();
-    const {data,isLoading,error,refetch}=useGetProductsQuery({pageNumber})
+  const { pageNumber } = useParams();
 
-    const [createProduct, { isLoading: loadingCreate }] =
+  const { data, isLoading, error, refetch } = useGetProductsQuery({
+    pageNumber,
+  });
+
+  const [deleteProduct, { isLoading: loadingDelete }] =
+    useDeleteProductMutation();
+
+  const deleteHandler = async (id) => {
+    if (window.confirm('Are you sure')) {
+      try {
+        await deleteProduct(id);
+        refetch();
+      } catch (err) {
+        toast.error(err?.data?.message || err.error);
+      }
+    }
+  };
+
+  const [createProduct, { isLoading: loadingCreate }] =
     useCreateProductMutation();
-
-    const [deleteProduct , {isLoading : loadingDelete}] = useDeleteProductMutation();
 
   const createProductHandler = async () => {
     if (window.confirm('Are you sure you want to create a new product?')) {
@@ -29,35 +46,21 @@ const ProductListScreen = () => {
     }
   };
 
-
-
-  const deleteHandler = async (id) => {
-    if (window.confirm('Are you sure')) {
-      try {
-        await deleteProduct(id);
-        toast.success('Product Deleted')
-        refetch();
-      } catch (err) {
-        toast.error(err?.data?.message || err.error);
-      }
-    }
-  };
-
-    
-    return <>
-     <Row className='align-items-center'>
+  return (
+    <>
+      <Row className='align-items-center'>
         <Col>
           <h1>Products</h1>
         </Col>
         <Col className='text-end'>
-          <Button className='my-3'  variant='success' onClick={createProductHandler}>
+          <Button className='my-3' onClick={createProductHandler}>
             <FaPlus /> Create Product
           </Button>
         </Col>
       </Row>
+
       {loadingCreate && <Loader />}
       {loadingDelete && <Loader />}
-      
       {isLoading ? (
         <Loader />
       ) : error ? (
@@ -107,11 +110,8 @@ const ProductListScreen = () => {
           <Paginate pages={data.pages} page={data.page} isAdmin={true} />
         </>
       )}
-    
-    
-    
     </>
-
-}
+  );
+};
 
 export default ProductListScreen;
